a5fff47382c315429f4327177e6d81faf96d06d4
Shougo Matsushita <Shougo.Matsu@gmail.com>
2016-11-22 03:29:31 +0900

    Update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,3 +5,31 @@ end
 function cd
   builtin cd $argv; and ls
 end
+
+set symbol_prompt '~>'
+
+# Color theme
+set fish_color_autosuggestion '1bc8c8'  'yellow'
+set fish_color_command 'f820ff'  'purple'
+set fish_color_comment 'cc6666', 'red'
+set fish_color_cwd 'ff13ff', 'yellow'
+set fish_color_cwd_root 'ff6666', 'red'
+set fish_color_end '66ff66', 'green'
+set fish_color_error 'red'  '--bold'
+set fish_color_escape '1e95fd', 'cyan'
+set fish_color_history_current '1e95fd', 'cyan'
+set fish_color_host 'c0c0c0', 'normal'
+set fish_color_match '1e95fd', 'cyan'
+set fish_color_normal '6c6c6c', 'normal'
+set fish_color_operator '1e95fd', 'cyan'
+set fish_color_param '00afff'  'cyan'
+set fish_color_quote 'f820ff', 'brown'
+set fish_color_redirection '6c6c6c', 'normal'
+set fish_color_search_match --background=purple
+set fish_color_selection --background=purple
+set fish_color_user '66ff66', 'green'
+set fish_color_valid_path --underline
+set fish_pager_color_completion '6c6c6c', 'normal'
+set fish_pager_color_description '555'  'yellow'
+set fish_pager_color_prefix '00afff'  'cyan'
+set fish_pager_color_progress '00afff'  'cyan'
