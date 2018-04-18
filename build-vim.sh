0cb01eb90d2443bbc4a4f49b6e358ab4236e5d14
Shougo Matsushita <Shougo.Matsu@gmail.com>
2018-04-18 23:01:49 +0900

    Update build-vim.sh

--- build-vim.sh
+++ build-vim.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 
-#CFLAGS='-g -O0' CPPFLAGS='-I/usr/include/python3.3m -I/usr/include/i386-linux-gnu/python3.3m' \
-#CPPFLAGS='-I/usr/include/python3.3m -I/usr/include/i386-linux-gnu/python3.3m' \
 ./configure \
 --with-features=huge \
 --with-compiledby="Shougo" \
 --enable-multibyte \
---enable-gui=gtk2 \
+--enable-gui=gtk3 \
 --enable-rubyinterp \
 --enable-python3interp \
---with-python3-config-dir=/usr/lib/python3.3/config-3.3m-i386-linux-gnu \
 --enable-luainterp \
 --with-lua-prefix=/usr --with-luajit \
 --enable-gpm \
