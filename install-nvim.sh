8cdf945204269e794c970d446af11ad6572a6f43
Shougo Matsushita <Shougo.Matsu@gmail.com>
2021-09-08 09:18:25 +0900

    Fix install

--- install-nvim.sh
+++ install-nvim.sh
@@ -4,7 +4,18 @@ make install
 
 # Remove default plugins
 if [ -d /usr/local/share/nvim/runtime/plugin ]; then
-    rm -Rf /usr/local/share/nvim/runtime/plugin
+    rm /usr/local/share/nvim/runtime/plugin/gzip.vim
+    rm /usr/local/share/nvim/runtime/plugin/health.vim
+    rm /usr/local/share/nvim/runtime/plugin/man.vim
+    rm /usr/local/share/nvim/runtime/plugin/matchit.vim
+    rm /usr/local/share/nvim/runtime/plugin/matchparen.vim
+    rm /usr/local/share/nvim/runtime/plugin/netrwPlugin.vim
+    rm /usr/local/share/nvim/runtime/plugin/shada.vim
+    rm /usr/local/share/nvim/runtime/plugin/spellfile.vim
+    rm /usr/local/share/nvim/runtime/plugin/tarPlugin.vim
+    rm /usr/local/share/nvim/runtime/plugin/tohtml.vim
+    rm /usr/local/share/nvim/runtime/plugin/tutor.vim
+    rm /usr/local/share/nvim/runtime/plugin/zipPlugin.vim
 fi
 if [ -d /usr/share/vim/vimfiles/plugin ]; then
     rm -Rf /usr/share/vim/vimfiles/plugin
