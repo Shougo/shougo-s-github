55b7e0f8397d0b3aeec2215dfca5093388f584f6
Shougo Matsushita <Shougo.Matsu@gmail.com>
2022-11-15 14:03:03 +0900

    Update

--- vim/after/queries/toml/highlights.scm
+++ vim/after/queries/toml/highlights.scm
@@ -7,8 +7,20 @@
 ((pair
   (bare_key) @_key
   (string) @none)
+ (#vim-match? @_key "^hook_\w*")
+ (#vim-match? @none "^('[^']|\"[^\"])")
+ (#offset! @none 0 1 0 -1))
+((pair
+  (bare_key) @_key
+  (string) @none)
  (#vim-match? @_key "^lua_\w*")
  (#offset! @none 0 3 0 -3))
+((pair
+  (bare_key) @_key
+  (string) @none)
+ (#vim-match? @_key "^lua_\w*")
+ (#vim-match? @none "^('[^']|\"[^\"])")
+ (#offset! @none 0 1 0 -1))
 ((table
   (bare_key) @_key
   (pair
--- vim/after/queries/toml/injections.scm
+++ vim/after/queries/toml/injections.scm
@@ -6,9 +6,21 @@
  (#offset! @vim 0 3 0 -3))
 ((pair
   (bare_key) @_key
+  (string) @vim)
+ (#vim-match? @_key "^hook_\w*")
+ (#vim-match? @vim "^('[^']|\"[^\"])")
+ (#offset! @vim 0 1 0 -1))
+((pair
+  (bare_key) @_key
   (string) @lua)
  (#vim-match? @_key "^lua_\w*")
  (#offset! @lua 0 3 0 -3))
+((pair
+  (bare_key) @_key
+  (string) @lua)
+ (#vim-match? @_key "^lua_\w*")
+ (#vim-match? @lua "^('[^']|\"[^\"])")
+ (#offset! @lua 0 1 0 -1))
 ((table
   (bare_key) @_key
   (pair
