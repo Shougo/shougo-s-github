afab8f3abf81c8d4fc2f53bbf38734684c943e61
Shougo Matsushita <Shougo.Matsu@gmail.com>
2023-06-04 10:04:33 +0900

    Update for TypeScript config

---
+++ vim/rc/ddc.ts
@@ -0,0 +1,202 @@
+import {
+  BaseConfig,
+  ContextBuilder,
+} from "https://deno.land/x/ddc_vim@v3.5.0/types.ts";
+import { Denops, fn } from "https://deno.land/x/ddc_vim@v3.5.0/deps.ts";
+
+export class Config extends BaseConfig {
+  // deno-lint-ignore require-await
+  override async config(args: {
+    denops: Denops;
+    contextBuilder: ContextBuilder;
+  }): Promise<void> {
+    args.contextBuilder.patchGlobal({
+      ui: "pum",
+      sources: ["codeium", "around", "file"],
+      autoCompleteEvents: [
+        "InsertEnter",
+        "TextChangedI",
+        "TextChangedP",
+        "CmdlineEnter",
+        "CmdlineChanged",
+        "TextChangedT",
+      ],
+      cmdlineSources: {
+        ":": ["cmdline", "cmdline-history", "around"],
+        "@": ["input", "cmdline-history", "file", "around"],
+        ">": ["input", "cmdline-history", "file", "around"],
+        "/": ["around", "line"],
+        "?": ["around", "line"],
+        "-": ["around", "line"],
+        "=": ["input"],
+      },
+      sourceOptions: {
+        _: {
+          ignoreCase: true,
+          matchers: ["matcher_head", "matcher_length"],
+          sorters: ["sorter_rank"],
+          converters: [
+            "converter_remove_overlap",
+            "converter_truncate_abbr",
+          ],
+          timeout: 1000,
+        },
+        around: {
+          mark: "A",
+        },
+        buffer: {
+          mark: "B",
+        },
+        necovim: {
+          mark: "vim",
+        },
+        "nvim-lua": {
+          mark: "lua",
+          forceCompletionPattern: "\.\w*",
+        },
+        cmdline: {
+          mark: "cmdline",
+          forceCompletionPattern: "\S/\S*|\.\w*",
+        },
+        copilot: {
+          mark: "cop",
+          matchers: [],
+          minAutoCompleteLength: 0,
+          isVolatile: false,
+        },
+        codeium: {
+          mark: "cod",
+          matchers: ["matcher_length"],
+          minAutoCompleteLength: 0,
+          isVolatile: true,
+        },
+        input: {
+          mark: "input",
+          forceCompletionPattern: "\S/\S*",
+          isVolatile: true,
+        },
+        line: {
+          mark: "line",
+          matchers: ["matcher_vimregexp"],
+        },
+        mocword: {
+          mark: "mocword",
+          minAutoCompleteLength: 4,
+          isVolatile: true,
+        },
+        "nvim-lsp": {
+          mark: "lsp",
+          forceCompletionPattern: "\.\w*|::\w*|->\w*",
+          dup: "force",
+        },
+        rtags: {
+          mark: "R",
+          forceCompletionPattern: "\.\w*|::\w*|->\w*",
+        },
+        file: {
+          mark: "F",
+          isVolatile: true,
+          minAutoCompleteLength: 1000,
+          forceCompletionPattern: "\S/\S*",
+        },
+        "cmdline-history": {
+          mark: "history",
+          sorters: [],
+        },
+        "shell-history": {
+          mark: "history",
+        },
+        shell: {
+          mark: "shell",
+          isVolatile: true,
+          forceCompletionPattern: "\S/\S*",
+        },
+        zsh: {
+          mark: "zsh",
+          isVolatile: true,
+          forceCompletionPattern: "\S/\S*",
+        },
+        rg: {
+          mark: "rg",
+          minAutoCompleteLength: 5,
+          enabledIf: "finddir('.git', ';') != ''",
+        },
+        skkeleton: {
+          mark: "skk",
+          matchers: ["skkeleton"],
+          sorters: [],
+          minAutoCompleteLength: 2,
+          isVolatile: true,
+        },
+      },
+      sourceParams: {
+        buffer: {
+          requireSameFiletype: false,
+          limitBytes: 50000,
+          fromAltBuf: true,
+          forceCollect: true,
+        },
+        file: {
+          filenameChars: "[:keyword:].",
+        },
+      },
+    });
+
+    for (
+      const filetype of [
+        "help",
+        "vimdoc",
+        "markdown",
+        "gitcommit",
+        "comment",
+      ]
+    ) {
+      args.contextBuilder.patchFiletype(filetype, {
+        sources: ["around", "codeium", "mocword"],
+      });
+    }
+
+    for (const filetype of ["html", "css"]) {
+      args.contextBuilder.patchFiletype(filetype, {
+        keywordPattern: "[0-9a-zA-Z_:#-]*",
+      });
+    }
+
+    const hasWindows = await fn.has(args.denops, "win32");
+    for (const filetype of ["zsh", "sh", "bash"]) {
+      args.contextBuilder.patchFiletype(filetype, {
+        keywordPattern: "[0-9a-zA-Z_./#:-]*",
+        sources: [hasWindows ? "shell" : "zsh", "around"],
+      });
+    }
+
+    args.contextBuilder.patchFiletype("ddu-ff-filter", {
+      keywordPattern: "[0-9a-zA-Z_:#-]*",
+      sources: ["line", "buffer"],
+      specialBufferCompletion: true,
+    });
+
+    const hasNvim = await fn.has(args.denops, "nvim");
+    if (hasNvim) {
+      for (
+        const filetype of [
+          "typescript",
+          "typescriptreact",
+          "go",
+          "python",
+          "css",
+          "html",
+          "ruby",
+        ]
+      ) {
+        args.contextBuilder.patchFiletype(filetype, {
+          sources: ["codeium", "nvim-lsp", "around"],
+        });
+      }
+
+      args.contextBuilder.patchFiletype("lua", {
+        sources: ["codeium", "nvim-lua", "around"],
+      });
+    }
+  }
+}
---
+++ vim/rc/ddu.ts
@@ -0,0 +1,170 @@
+import {
+  BaseConfig,
+  ContextBuilder,
+} from "https://deno.land/x/ddu_vim@v3.0.0/types.ts";
+import { Denops } from "https://deno.land/x/ddu_vim@v3.0.0/deps.ts";
+
+export class Config extends BaseConfig {
+  // deno-lint-ignore require-await
+  override async config(args: {
+    denops: Denops;
+    contextBuilder: ContextBuilder;
+  }): Promise<void> {
+    args.contextBuilder.patchGlobal({
+      ui: "ff",
+      uiOptions: {
+        filer: {
+          toggle: true,
+        },
+      },
+      uiParams: {
+        ff: {
+          filterSplitDirection: "floating",
+          floatingBorder: "none",
+          previewFloating: true,
+          previewFloatingBorder: "single",
+          previewSplit: "no",
+          highlights: {
+            floating: "Normal",
+            floatingBorder: "Special",
+          },
+          updateTime: 0,
+          winWidth: 100,
+        },
+        filer: {
+          split: "no",
+          sort: "filename",
+          sortTreesFirst: true,
+          toggle: true,
+        },
+      },
+      sourceOptions: {
+        _: {
+          ignoreCase: true,
+          matchers: ["matcher_substring"],
+        },
+        file_old: {
+          matchers: [
+            "matcher_substring",
+            "matcher_relative",
+            "matcher_ignore_current_buffer",
+          ],
+        },
+        file_external: {
+          matchers: [
+            "matcher_substring",
+          ],
+        },
+        file_rec: {
+          matchers: [
+            "matcher_substring",
+            "matcher_hidden",
+          ],
+        },
+        file: {
+          matchers: [
+            "matcher_substring",
+            "matcher_hidden",
+          ],
+          sorters: ["sorter_alpha"],
+        },
+        dein: {
+          defaultAction: "cd",
+        },
+        markdown: {
+          sorters: [],
+        },
+        line: {
+          matchers: [
+            "matcher_kensaku",
+          ],
+        },
+        path_history: {
+          defaultAction: "uiCd",
+        },
+        rg: {
+          matchers: [
+            "matcher_substring",
+            "matcher_files",
+          ],
+        },
+      },
+      sourceParams: {
+        file_external: {
+          cmd: ["git", "ls-files", "-co", "--exclude-standard"],
+        },
+        rg: {
+          args: [
+            "--ignore-case",
+            "--column",
+            "--no-heading",
+            "--color",
+            "never",
+          ],
+        },
+        file_rg: {
+          cmd: [
+            "rg",
+            "--files",
+            "--glob",
+            "!.git",
+            "--color",
+            "never",
+            "--no-messages",
+          ],
+          updateItems: 50000,
+        },
+      },
+      filterParams: {
+        matcher_kensaku: {
+          highlightMatched: "Search",
+        },
+        matcher_substring: {
+          highlightMatched: "Search",
+        },
+        matcher_ignore_files: {
+          ignoreGlobs: ["test_*.vim"],
+          ignorePatterns: [],
+        },
+      },
+      kindOptions: {
+        file: {
+          defaultAction: "open",
+        },
+        word: {
+          defaultAction: "append",
+        },
+        deol: {
+          defaultAction: "switch",
+        },
+        action: {
+          defaultAction: "do",
+        },
+        readme_viewer: {
+          defaultAction: "open",
+        },
+      },
+      kindParams: {
+        action: {
+          quit: true,
+        },
+      },
+      actionOptions: {
+        narrow: {
+          quit: false,
+        },
+        tabopen: {
+          quit: false,
+        },
+      },
+    });
+
+    args.contextBuilder.patchLocal("files", {
+      uiParams: {
+        ff: {
+          split: "floating",
+        },
+      },
+    });
+  }
+}
