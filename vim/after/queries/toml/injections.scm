7e98ab8def2a1eacbe61b71ae23753f9c878fb93
Shougo Matsushita <Shougo.Matsu@gmail.com>
2023-08-14 10:56:59 +0900

    Fix injections

--- vim/after/queries/toml/injections.scm
+++ vim/after/queries/toml/injections.scm
@@ -1,35 +1,41 @@
 ;; extends
 ((pair
   (bare_key) @_key
-  (string) @vim)
+  (string) @injection.content)
  (#vim-match? @_key "^hook_\w*")
- (#offset! @vim 0 3 0 -3))
+ (#set! injection.language "vim")
+ (#offset! @injection.content 0 3 0 -3))
 ((pair
   (bare_key) @_key
-  (string) @vim)
+  (string) @injection.content)
  (#vim-match? @_key "^hook_\w*")
- (#vim-match? @vim "^('[^']|\"[^\"])")
- (#offset! @vim 0 1 0 -1))
+ (#vim-match? @injection.content "^('[^']|\"[^\"])")
+ (#set! injection.language "vim")
+ (#offset! @injection.content 0 1 0 -1))
 ((pair
   (bare_key) @_key
-  (string) @lua)
+  (string) @injection.content)
  (#vim-match? @_key "^lua_\w*")
- (#offset! @lua 0 3 0 -3))
+ (#set! injection.language "lua")
+ (#offset! @injection.content 0 3 0 -3))
 ((pair
   (bare_key) @_key
-  (string) @lua)
+  (string) @injection.content)
  (#vim-match? @_key "^lua_\w*")
- (#vim-match? @lua "^('[^']|\"[^\"])")
- (#offset! @lua 0 1 0 -1))
+ (#vim-match? @injection.content "^('[^']|\"[^\"])")
+ (#set! injection.language "lua")
+ (#offset! @injection.content 0 1 0 -1))
 ((table
   (bare_key) @_key
   (pair
-   (string) @vim))
+   (string) @injection.content))
  (#eq? @_key "ftplugin")
- (#offset! @vim 0 3 0 -3))
+ (#set! injection.language "vim")
+ (#offset! @injection.content 0 3 0 -3))
 ((table
   (dotted_key) @_key
   (pair
-   (string) @vim))
+   (string) @injection.content))
  (#eq? @_key "plugins.ftplugin")
- (#offset! @vim 0 3 0 -3))
+ (#set! injection.language "vim")
+ (#offset! @injection.content 0 3 0 -3))
