039e18e094b63ca0a2640db065a5ccd2f5251afb
Shougo Matsushita <Shougo.Matsu@gmail.com>
2023-09-19 12:15:30 +0900

    Add dpp config

--- vim/rc/dpp.ts
+++ vim/rc/dpp.ts
@@ -1,8 +1,36 @@
 import { BaseConfig } from "https://deno.land/x/ddc_vim@v4.0.4/types.ts";
-import { ConfigArguments } from "https://deno.land/x/ddc_vim@v4.0.4/base/config.ts";
+import { Denops } from "https://deno.land/x/ddc_vim@v4.0.4/deps.ts";
 
 export class Config extends BaseConfig {
-  override async config(args: ConfigArguments): Promise<void> {
-    //console.log(args);
+  override async config(args: {
+    denops: Denops;
+    basePath: string;
+    dpp: Dpp;
+  }): Promise<Plugin[]> {
+    let plugins: Plugin[] = []
+
+    plugins = plugins.concat(await args.dpp.extAction(args.denops, "local", "local", {
+      directory: "~/work",
+      options: {
+        frozen: true,
+        merged: false,
+      },
+      includes: [
+        "vim*", "nvim-*", "*.vim", "*.nvim",
+        "ddc-*", "ddu-*",
+        "skkeleton", "neco-vim",
+      ],
+    }));
+
+    plugins = plugins.concat(await args.dpp.extAction(args.denops, "toml", "load", {
+      path: "$BASE_DIR/dein.toml",
+      options: {
+        lazy: true,
+      },
+    }));
+
+    console.log(plugins);
+
+    return plugins;
   }
 }
