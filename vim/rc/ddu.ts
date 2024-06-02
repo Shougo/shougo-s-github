26ac9463867fa659728a1e5adbd8fcbc1e928d90
Shougo Matsushita <Shougo.Matsu@gmail.com>
2024-06-02 11:15:32 +0900

    Improve

--- vim/rc/ddu.ts
+++ vim/rc/ddu.ts
@@ -6,7 +6,7 @@ import {
 } from "https://deno.land/x/ddu_vim@v4.1.0/types.ts";
 import { Denops, fn } from "https://deno.land/x/ddu_vim@v4.1.0/deps.ts";
 import { ConfigArguments } from "https://deno.land/x/ddu_vim@v4.1.0/base/config.ts";
-import { ActionData } from "https://deno.land/x/ddu_kind_file@v0.7.1/file.ts";
+import { ActionData as FileAction } from "https://deno.land/x/ddu_kind_file@v0.7.1/file.ts";
 import { Params as FfParams } from "https://deno.land/x/ddu_ui_ff@v1.1.0/ff.ts";
 import { Params as FilerParams } from "https://deno.land/x/ddu_ui_filer@v1.1.0/filer.ts";
 
@@ -207,7 +207,7 @@ export class Config extends BaseConfig {
           defaultAction: "open",
           actions: {
             grep: async (args: ActionArguments<Params>) => {
-              const action = args.items[0]?.action as ActionData;
+              const action = args.items[0]?.action as FileAction;
 
               await args.denops.call("ddu#start", {
                 name: args.options.name,
