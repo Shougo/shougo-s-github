233f4294f5c21ae005aa64771bada6eeba6f454c
Shougo Matsushita <Shougo.Matsu@gmail.com>
2024-08-29 09:52:31 +0900

    Use ddu.vim version 6

--- vim/rc/ddu.ts
+++ vim/rc/ddu.ts
@@ -2,11 +2,11 @@ import {
   type ActionArguments,
   ActionFlags,
   type DduOptions,
-} from "jsr:@shougo/ddu-vim@~6.0.0-pre1/types";
+} from "jsr:@shougo/ddu-vim@~6.0.0/types";
 import {
   BaseConfig,
   type ConfigArguments,
-} from "jsr:@shougo/ddu-vim@~6.0.0-pre1/config";
+} from "jsr:@shougo/ddu-vim@~6.0.0/config";
 import { type ActionData as FileAction } from "jsr:@shougo/ddu-kind-file@~0.8.0";
 import { type Params as FfParams } from "jsr:@shougo/ddu-ui-ff@~1.2.0";
 import { type Params as FilerParams } from "jsr:@shougo/ddu-ui-filer@~1.2.0";
