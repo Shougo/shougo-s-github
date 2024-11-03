e9bd331e4eea2244d34d87c2a7c90959c09d536a
Shougo Matsushita <Shougo.Matsu@gmail.com>
2024-11-03 11:16:05 +0900

    Update deps

--- vim/rc/ddu.ts
+++ vim/rc/ddu.ts
@@ -2,11 +2,11 @@ import {
   type ActionArguments,
   ActionFlags,
   type DduOptions,
-} from "jsr:@shougo/ddu-vim@~6.2.0/types";
+} from "jsr:@shougo/ddu-vim@~6.4.0/types";
 import {
   BaseConfig,
   type ConfigArguments,
-} from "jsr:@shougo/ddu-vim@~6.2.0/config";
+} from "jsr:@shougo/ddu-vim@~6.4.0/config";
 import { type ActionData as FileAction } from "jsr:@shougo/ddu-kind-file@~0.9.0";
 import { type Params as FfParams } from "jsr:@shougo/ddu-ui-ff@~1.4.0";
 import { type Params as FilerParams } from "jsr:@shougo/ddu-ui-filer@~1.4.0";
