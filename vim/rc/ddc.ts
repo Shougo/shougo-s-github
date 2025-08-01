afe469f57b640d2f2589915d6646486c7fc63136
Shougo Matsushita <Shougo.Matsu@gmail.com>
2025-08-01 14:06:49 +0900

    Update

--- vim/rc/ddc.ts
+++ vim/rc/ddc.ts
@@ -1,8 +1,8 @@
-import { BaseConfig, ConfigArguments } from "jsr:@shougo/ddc-vim@~9.5.0/config";
-import type { Context, DdcItem } from "jsr:@shougo/ddc-vim@~9.5.0/types";
+import { BaseConfig, ConfigArguments } from "@shougo/ddc-vim/config";
+import type { Context, DdcItem } from "@shougo/ddc-vim/types";
 
-import type { Denops } from "jsr:@denops/std@~7.6.0";
-import * as fn from "jsr:@denops/std@~7.6.0/function";
+import type { Denops } from "@denops/std";
+import * as fn from "@denops/std/function";
 
 export class Config extends BaseConfig {
   override async config(args: ConfigArguments): Promise<void> {
