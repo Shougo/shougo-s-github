02200be67a2eb6ae2cf0425da74eda0529c2ed52
Shougo Matsushita <Shougo.Matsu@gmail.com>
2025-08-24 16:35:33 +0900

    Use load_config() in ddx

---
+++ vim/rc/ddx.ts
@@ -0,0 +1,9 @@
+import { BaseConfig, ConfigArguments } from "@shougo/ddx-vim/config";
+
+export class Config extends BaseConfig {
+  override config(args: ConfigArguments): void {
+    args.contextBuilder.patchGlobal({
+      ui: "hex",
+    });
+  }
+}
